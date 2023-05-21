6a342c51392fe4928fe00fddbaa87969d290c054
balrogwhip23 <134221892+balrogwhip23@users.noreply.github.com>
2023-05-21 18:56:29 +0200

    Create task1.sh
    This is a basic script to be used to learn the creation of a cicd pipeline

---
+++ task1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh /media/piero/f3bbb7c0-1041-4414-a64e-d69e63961531/software/teamcity/TeamCity/buildAgent/work/b65e22cbb8c71e65
+# Basic example to compile
+
+echo "Master compilation step"
+make
